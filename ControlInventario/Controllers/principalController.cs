05031f4462e0d32e9c6f621ac5ba9efaf3aa3bdb
mariajsan <60493676+mariajsan@users.noreply.github.com>
2020-05-22 01:57:29 -0700

    edit

---
+++ ControlInventario/Controllers/principalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlInventario.Controllers
+{
+    [Authorize]
+    public class principalController : Controller
+    {
+        // GET: principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControlInventario/Models/productoscontextdb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class productoscontextdb : DbContext
+    {
+        public productoscontextdb() : base("MyConnection")
+        {
+        }
+        public DbSet<productos> Producto { get; set; }
+    }
+}
